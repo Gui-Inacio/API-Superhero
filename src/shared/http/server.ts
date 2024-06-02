897e609781e19a2f4af5c8701544a570f09c7713
Wellington Santos <well.santos1334@gmail.com>
2024-06-02 19:51:51 -0300

    fix: add errorHandler on server file and remove unsed imports

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,11 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
 import 'reflect-metadata';
+import 'express-async-errors';
+import express from 'express';
+import cors from 'cors';
 import '../container';
 
 import { userRouter } from '../../modules/user/infra/http/routes/users.routes';
+import { ErrorRequestHandler } from '../errors/error-handler';
 
 import routes from './routes';
 
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(routes);
 app.use(express.json());
 app.use('/users', userRouter);
+app.use(ErrorRequestHandler);
 // app.use((error: Error, request: Request, response:Response, next: NextFunction)=> {
 //   if (error instanceof AppError)
 // })
