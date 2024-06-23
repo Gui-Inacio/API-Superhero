a18f8f3babe3b1b39c055309c15b8d3310bdd583
Guilherme Inácio <44714868+Gui-Inacio@users.noreply.github.com>
2024-06-23 20:12:59 -0300

    feat: add auth in user search routes

--- src/modules/authentication/infra/http/controller/index.ts
+++ src/modules/authentication/infra/http/controller/index.ts
@@ -28,14 +28,14 @@ export class AuthenticationController {
     }
 
     const JwtToken = authHeader.split(' ')[1];
-    console.log(JwtToken);
+
     if (!JwtToken) {
       return response.status(401).json({ message: 'Token is missing' });
     }
 
     // Invalida o token adicionando-o à lista de sessões (blacklist)
     sessions[JwtToken] = false;
-    console.log(JwtToken);
+
     return response.status(204).send('Logout feito com sucesso!'); // No Content
   }
   async teste(request: Request, response: Response): Promise<Response> {
--- src/modules/user/infra/http/routes/users.routes.ts
+++ src/modules/user/infra/http/routes/users.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 
-import UsersController from './../controller/UsersController';
 import '../../../../../shared/container';
+import UsersController from './../controller/UsersController';
+
+import { isAuth } from '@/shared/infra/http/middlewares/IsAuth';
 
 const userRouter = Router();
 const userController = new UsersController();
 
 userRouter.post('/create', userController.createUser);
-userRouter.get('/search/:id', userController.findById);
-userRouter.get('/email/:email', userController.findByEmail);
-userRouter.get('/', userController.listAll);
+userRouter.get('/search/:id', isAuth, userController.findById);
+userRouter.get('/email/:email', isAuth, userController.findByEmail);
+userRouter.get('/', isAuth, userController.listAll);
 //userRouter.get('/logout');
 
 export { userRouter };
