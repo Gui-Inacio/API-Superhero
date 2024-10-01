4e1cd8959b809c43b434cccc50e8ae6c881d0f96
Guilherme Inácio <44714868+Gui-Inacio@users.noreply.github.com>
2024-09-30 21:59:25 -0300

    create AttributeRepository

---
+++ src/modules/superhero/infra/typeorm/repositories/AttributeRepository.ts
@@ -0,0 +1,39 @@
+import { injectable } from 'tsyringe';
+
+import { Attribute } from '../entities/Attribute';
+
+import { AbstractTransactionRepository } from '@/shared/container/providers/transaction-menager/AbstractTransactionRepository';
+import {
+  AttributeSaveInput,
+  AttributeUpdate,
+  IAttributeRepository,
+} from '@/modules/superhero/repositories/IAttributeRepository';
+import { AppDataSource } from '@/shared/infra/typeorm';
+import { TransactionManager } from '@/shared/container/providers/transaction-menager/TransactionManager';
+
+@injectable()
+export class AttributeRepository
+  extends AbstractTransactionRepository<Attribute>
+  implements IAttributeRepository
+{
+  private readonly attributeRepository = AppDataSource.getRepository(Attribute);
+  constructor(protected transaction: TransactionManager) {
+    super(transaction, Attribute);
+  }
+  async create(data: AttributeSaveInput) {
+    const attribute = this.attributeRepository.create(data);
+    return await this.attributeRepository.save(attribute);
+  }
+  async findById(id: string) {
+    return await this.attributeRepository.findOne({ where: { id } });
+  }
+  async listAll() {
+    return await this.attributeRepository.find();
+  }
+  async update(data: AttributeUpdate) {
+    await this.attributeRepository.update({ id: data.id }, data);
+  }
+  async delete(id: string) {
+    await this.attributeRepository.delete(id);
+  }
+}
--- src/modules/superhero/repositories/IAttributeRepository.ts
+++ src/modules/superhero/repositories/IAttributeRepository.ts
@@ -4,13 +4,14 @@ import { StrictOmit } from '@/shared/util/types/StrictOmitType';
 
 export type AttributeSaveInput = StrictOmit<Attribute, 'id'>;
 
-//export type SuperheroUpdateInput = Pick<Superhero, 'id'>;
+export type AttributeUpdate = StrictOmit<Attribute, 'heroAttributes'>;
 
 interface IAttributeRepository {
   create(data: AttributeSaveInput): Promise<Attribute>;
   findById(id: string): Promise<Attribute | null>;
   listAll(): Promise<Attribute[]>;
   delete(id: string): Promise<void>;
+  update(data: AttributeUpdate): Promise<void>;
 }
 
 export { IAttributeRepository };
