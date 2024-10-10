17729f4daaba47db17b377c67c01211ca2d0cc8c
Guilherme Inácio <44714868+Gui-Inacio@users.noreply.github.com>
2024-10-10 19:02:37 -0300

    add HeroAttribute Repository

---
+++ src/modules/superhero/infra/typeorm/repositories/HeroAttributeRepository.ts
@@ -0,0 +1,44 @@
+import { injectable } from 'tsyringe';
+
+import { HeroAttribute } from '../entities/HeroAttribute';
+
+import { AbstractTransactionRepository } from '@/shared/container/providers/transaction-menager/AbstractTransactionRepository';
+import {
+  HeroAttributeSaveInput,
+  HeroAttributeUpdate,
+  IHeroAttributeRepository,
+} from '@/modules/superhero/repositories/IHeroAttributeRepository';
+import { AppDataSource } from '@/shared/infra/typeorm';
+import { TransactionManager } from '@/shared/container/providers/transaction-menager/TransactionManager';
+
+@injectable()
+export class HeroAttributeRepositoy
+  extends AbstractTransactionRepository<HeroAttribute>
+  implements IHeroAttributeRepository
+{
+  private readonly heroAttributeRepository =
+    AppDataSource.getRepository(HeroAttribute);
+  constructor(protected transaction: TransactionManager) {
+    super(transaction, HeroAttribute);
+  }
+
+  async create(data: HeroAttributeSaveInput) {
+    const heroAttribute = this.heroAttributeRepository.create(data);
+    return await this.heroAttributeRepository.save(heroAttribute);
+  }
+
+  async findById(id: string) {
+    return await this.heroAttributeRepository.findOne({ where: { id } });
+  }
+  async listAll() {
+    return await this.heroAttributeRepository.find();
+  }
+
+  async update(data: HeroAttributeUpdate) {
+    await this.heroAttributeRepository.update({ id: data.id }, data);
+  }
+
+  async delete(id: string) {
+    await this.heroAttributeRepository.delete(id);
+  }
+}
--- src/modules/superhero/repositories/IHeroAttributeRepository.ts
+++ src/modules/superhero/repositories/IHeroAttributeRepository.ts
@@ -4,13 +4,17 @@ import { StrictOmit } from '@/shared/util/types/StrictOmitType';
 
 export type HeroAttributeSaveInput = StrictOmit<HeroAttribute, 'id'>;
 
-//export type SuperheroUpdateInput = Pick<Superhero, 'id'>;
+export type HeroAttributeUpdate = Pick<
+  HeroAttribute,
+  'id' | 'attribute_value' | 'superhero' | 'attribute'
+>;
 
 interface IHeroAttributeRepository {
   create(data: HeroAttributeSaveInput): Promise<HeroAttribute>;
   findById(id: string): Promise<HeroAttribute | null>;
   listAll(): Promise<HeroAttribute[] | null | HeroAttribute>;
   delete(id: string): Promise<void>;
+  update(data: HeroAttributeUpdate): Promise<void>;
 }
 
 export { IHeroAttributeRepository };
